187170c644128431a166c728ce7178dd32050846
Kathleen Faria <kathleenribeiro350@gmail.com>
2025-05-28 21:37:42 -0300

    iniciando as configurações do projeto

---
+++ src/src/main/java/br/edu/go/puc/ads/Main.java
@@ -0,0 +1,12 @@
+package br.edu.go.puc.ads;
+
+/**
+ *
+ * @author Kathleen Faria 🌻
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
