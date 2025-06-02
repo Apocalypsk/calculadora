144de3d910785bc42dcf63aede498fb90235a385
Kathleen Faria <kathleenribeiro350@gmail.com>
2025-06-02 00:51:07 -0300

    Criada a base do projeto

---
+++ src/src/main/java/br/edu/go/puc/ads/model/ExpressaoAlgebrica.java
@@ -0,0 +1,34 @@
+package br.edu.go.puc.ads.model;
+
+public class ExpressaoAlgebrica {
+
+    private Double primeiroValor;
+    private Double segundoValor;
+    private OperacaoAlgebrica operacao;
+
+    
+    public Double getPrimeiroValor() {
+        return primeiroValor;
+    }
+
+    public void setPrimeiroValor(Double primeiroValor) {
+        this.primeiroValor = primeiroValor;
+    }
+
+    public Double getSegundoValor() {
+        return segundoValor;
+    }
+
+    public void setSegundoValor(Double segundoValor) {
+        this.segundoValor = segundoValor;
+    }
+
+    public OperacaoAlgebrica getOperacao() {
+        return operacao;
+    }
+
+    public void setOperacao(OperacaoAlgebrica operacao) {
+        this.operacao = operacao;
+    }
+
+}
---
+++ src/src/main/java/br/edu/go/puc/ads/model/OperacaoAlgebrica.java
@@ -0,0 +1,8 @@
+package br.edu.go.puc.ads.model;
+
+public enum OperacaoAlgebrica {
+    SOMA,
+    SUBTRACAO,
+    MULTIPLICACAO,
+    DIVISAO
+}
