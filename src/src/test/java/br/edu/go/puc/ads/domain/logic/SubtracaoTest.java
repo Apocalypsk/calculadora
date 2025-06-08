84f5f0e044e4eca29f1811311b59dcc15e0a2421
Kathleen Faria <kathleenribeiro350@gmail.com>
2025-06-08 15:23:27 -0300

    correção ortográfica

--- src/src/test/java/br/edu/go/puc/ads/domain/logic/SubtracaoTest.java
+++ src/src/test/java/br/edu/go/puc/ads/domain/logic/SubtracaoTest.java
@@ -6,21 +6,21 @@ import org.junit.jupiter.api.Test;
 class SubtracaoTest {
 
     @Test
-    void testSubrairPositivos() {
+    void testSubtrairPositivos() {
         Subtracao subtracao = new Subtracao(2.0, 1.0);
         double resultado = subtracao.getSubtracao();
         assertEquals(1.0, resultado, "A subtração deveria ser 1.0");
     }
 
     @Test
-    void testSubrairNegativos(){
+    void testSubtrairNegativos(){
         Subtracao subtracao = new Subtracao( -2.0, -1.0);
         double resultado = subtracao.getSubtracao();
         assertEquals( -1.0, resultado, "A subtração deveria ser -1.0");
     }
     
     @Test
-    void testSubrairZero(){
+    void testSubtrairZero(){
         Subtracao subtracao = new Subtracao( 0.0, 0.0);
         double resultado = subtracao.getSubtracao();
         assertEquals( 0.0, resultado, "A subtração deveria ser 0.0");
